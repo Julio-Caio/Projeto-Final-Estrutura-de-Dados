c9d0ba5dd4a6f13e942c8854407fed75d9db2d5c
Júlio Caio <101609625+Julio-Caio@users.noreply.github.com>
2023-12-18 10:41:27 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,10 +5,9 @@ from Menu import Menu
 
 if __name__ == "__main__":
     menu = Menu()
-    email = input("Digite seu e-mail: ")
 
     print(f'''
-    Bem-Vindo ao Gerenciador de Jogos, {email}! Como deseja entrar?
+    Bem-Vindo ao Gerenciador de Jogos! Como deseja entrar?
     ===================================
     | (1) Entrar com token de acesso   |
     ===================================
@@ -18,10 +17,12 @@ if __name__ == "__main__":
 
     database = Database()
     opcao = int(input('> Escolha sua opção de login: '))
-    user = User(email)
-    database.cadastrar(user, opcao)
     
     if opcao == 1:
+        email = input("Digite seu e-mail: ")
+        user = User(email)
+        database.cadastrar(user, opcao)
+
         token = int(input("\nDigite o token de acesso: "))
         if database.validar_token(token):
             menu()
